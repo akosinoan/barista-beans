b3e18128bb3e0fb092dd2fdd84dec22b14372b3c
akosinoan <noan.babao@gmail.com>
2020-08-05 18:24:31 +0200

    Updates

---
+++ precache-manifest.17070ca78d8635245ca8a8fa875f2581.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327cf85404ba909a6816af31c7e4a44a",
+    "url": "/barista-beans/index.html"
+  },
+  {
+    "revision": "dd76fcf5d602eaa9942b",
+    "url": "/barista-beans/static/css/main.d7101303.chunk.css"
+  },
+  {
+    "revision": "91640cee6554b16cc3c4",
+    "url": "/barista-beans/static/js/2.c4cb37b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/barista-beans/static/js/2.c4cb37b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd76fcf5d602eaa9942b",
+    "url": "/barista-beans/static/js/main.ce78892a.chunk.js"
+  },
+  {
+    "revision": "fe7691dbfa26467c122a",
+    "url": "/barista-beans/static/js/runtime-main.dfd79028.js"
+  }
+]);
